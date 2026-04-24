f974ffe23bf2ac78bc9c0e2c99a728f265b50d99
Johnyol <dev18.seucondominio@gmail.com>
2026-04-24 14:36:41 -0300

    Adicionando CRUD insumos

--- app/Infrastructure/Providers/AppServiceProvider.php
+++ app/Infrastructure/Providers/AppServiceProvider.php
@@ -6,12 +6,14 @@ use App\Domain\OrdemServico\Repositories\OrdemServicoRepositoryInterface;
 use App\Domain\Notificacao\Repositories\NotificacaoRepositoryInterface;
 use App\Domain\Cliente\Repositories\ClienteRepositoryInterface;
 use App\Domain\Peca\Repositories\PecaRepositoryInterface;
+use App\Domain\Insumo\Repositories\InsumoRepositoryInterface;
 use App\Domain\User\Repositories\UserRepositoryInterface;
 use App\Domain\Veiculo\Repositories\VeiculoRepositoryInterface;
 use App\Infrastructure\Persistence\Eloquent\Repositories\EloquentNotificacaoRepository;
 use App\Infrastructure\Persistence\Eloquent\Repositories\EloquentOrdemServicoRepository;
 use App\Infrastructure\Persistence\Eloquent\Repositories\EloquentClienteRepository;
 use App\Infrastructure\Persistence\Eloquent\Repositories\EloquentPecaRepository;
+use App\Infrastructure\Persistence\Eloquent\Repositories\EloquentInsumoRepository;
 use App\Infrastructure\Persistence\Eloquent\Repositories\EloquentUserRepository;
 use App\Infrastructure\Persistence\Eloquent\Repositories\EloquentVeiculoRepository;
 use Illuminate\Support\ServiceProvider;
@@ -44,6 +46,11 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(
             PecaRepositoryInterface::class,
             EloquentPecaRepository::class
+        );
+
+        $this->app->bind(
+            InsumoRepositoryInterface::class,
+            EloquentInsumoRepository::class
         );
 
         $this->app->bind(
