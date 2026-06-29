0698e536e6518951e82356aca7474139165db0a7
dtavares101 <119620612+dtavares101@users.noreply.github.com>
2026-06-29 13:59:37 -0300

    feat(fase 2):implementação ci/cd

--- docker/start.sh
+++ docker/start.sh
@@ -2,6 +2,7 @@
 
 # Start PHP-FPM in background
 # Set permissions
+mkdir -p /var/www/html/storage/framework/views /var/www/html/storage/framework/cache /var/www/html/storage/framework/sessions
 chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
 
 # Run migrations
