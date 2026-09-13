714bdf6575a8ae460cbbb155296d42367d567c13
David Tavares <48646679+dvdt101@users.noreply.github.com>
2026-09-13 20:23:17 -0300

    feat: agente nativo do New Relic (APM) junto com OTel
    - Dockerfile: instala newrelic-php5 no estagio de producao
    - start.sh: injeta NEW_RELIC_LICENSE_KEY/APP_NAME no ini em boot
    - configmap: NEW_RELIC_APP_NAME ao lado das vars de OTel ja existentes
    - secret.example: NEW_RELIC_LICENSE_KEY
    - imagem: 2.2 -> 2.3

--- docker/start.sh
+++ docker/start.sh
@@ -5,6 +5,12 @@
 mkdir -p /var/www/html/storage/framework/views /var/www/html/storage/framework/cache /var/www/html/storage/framework/sessions
 chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
 
+# New Relic: injeta license key no ini
+if [ -n "$NEW_RELIC_LICENSE_KEY" ] && [ -f /usr/local/etc/php/conf.d/newrelic.ini ]; then
+  sed -i "s/^newrelic.license = .*/newrelic.license = \"${NEW_RELIC_LICENSE_KEY}\"/" /usr/local/etc/php/conf.d/newrelic.ini
+  sed -i "s/^newrelic.appname = .*/newrelic.appname = \"${NEW_RELIC_APP_NAME:-oficina-mecanica-api}\"/" /usr/local/etc/php/conf.d/newrelic.ini
+fi
+
 # Run migrations
 php artisan migrate --force
 
